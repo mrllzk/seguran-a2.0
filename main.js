ba01142b8709c50b0c927b42a144b12d636f58e1
mrllzk <murillo.silva.pereira@escola.pr.gov.br>
2026-07-03 11:29:28 -0300

    aula 4

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 12;
-numeroSenha.textContent = tamanhoSenha;
-
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+numeroSenha.textContent = tamanhoSenha;
+let tamanhoSenha = 12
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
-console.log(botoes)
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+       // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       // tamanhoSenha = tamanhoSenha+1;
+       tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
